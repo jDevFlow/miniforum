3f0b7af0d7679c3ffa619d7290911b3f2a88ef10
jDevFlow <prometei.agent@gmail.com>
2019-09-11 16:02:38 +0300

    code style

--- src/views/Voting.vue
+++ src/views/Voting.vue
@@ -1,58 +1,49 @@
 <template>
   <div>
-
     <div class="row">
         <div class="col s12 m6">
           <div class="card">
             <div class="card-content">
+              <h5>Ваш любимый язык программирования</h5>
+              <p>
+                <label>
+                  <img src="https://www.tutorialspoint.com/images/java8.png" alt=""  class="circle">
+                  <input class="with-gap" name="group1" type="radio" value="Java" v-model="votlang">
+                  <span>Java</span>
+                </label>
+              </p>
+              <p>
+                <label>
+                  <img src="https://www.tutorialspoint.com/images/Python.png" alt=""  class="circle">
+                  <input class="with-gap" name="group1" type="radio" value="Python" v-model="votlang">
+                  <span>Python</span>
+                </label>
+              </p>
+              <p>
+                <label>
+                  <img src="https://www.tutorialspoint.com/images/php.png" alt=""  class="circle">
 
-    <h5>Ваш любимый язык программирования</h5>
-    <p>
-      <label>
-        <img src="https://www.tutorialspoint.com/images/java8.png" alt=""  class="circle">
-        <input class="with-gap" name="group1" type="radio" value="Java" v-model="votlang">
-        <span>Java</span>
-      </label>
-    </p>
-    <p>
-      <label>
-        <img src="https://www.tutorialspoint.com/images/Python.png" alt=""  class="circle">
-        <input class="with-gap" name="group1" type="radio" value="Python" v-model="votlang">
-        <span>Python</span>
-      </label>
-    </p>
-    <p>
-      <label>
-        <img src="https://www.tutorialspoint.com/images/php.png" alt=""  class="circle">
-
-        <input class="with-gap" name="group1" type="radio" value="PHP" v-model="votlang">
-        <span>PHP</span>
-      </label>
-    </p>
-    <p>
-      <label>
-        <img src="https://www.tutorialspoint.com/images/Swift.png" alt=""  class="circle">
-        <input class="with-gap" name="group1" type="radio" value="Swift" v-model="votlang">
-        <span>Swift</span>
-      </label>
-    </p>
-    <div class="card-action">
-      <span class="left">Выбрано: {{ votlang }}</span>
-
-      <div class="right">
-        <button class="btn" >Голосовать</button>
-      </div>
-
-    </div>
-
-
-
-
+                  <input class="with-gap" name="group1" type="radio" value="PHP" v-model="votlang">
+                  <span>PHP</span>
+                </label>
+              </p>
+              <p>
+                <label>
+                  <img src="https://www.tutorialspoint.com/images/Swift.png" alt=""  class="circle">
+                  <input class="with-gap" name="group1" type="radio" value="Swift" v-model="votlang">
+                  <span>Swift</span>
+                </label>
+              </p>
+              <div class="card-action">
+                <span class="left">Выбрано: {{ votlang }}</span>
+                <div class="right">
+                  <a class="btn" >Голосовать</a>
+                </div>
+              </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-
   </div>
 </template>
 <script>
